b92fc98e6dd97f3a5647b15361dd5971b2ed2ded
liuziangexit <liuziang@liuziangexit.com>
2018-08-13 21:01:23 +0800

    更新多语言

--- Chinese-Word-Analyzer/MainWindow.xaml.cs
+++ Chinese-Word-Analyzer/MainWindow.xaml.cs
@@ -19,8 +19,10 @@ namespace Chinese_Word_Analyzer
     {
         public MainWindow()
         {
-            ResetLanguageResource(GetDefaultLanguageResource());
             InitializeComponent();
+            LoadRegionCodes();
+            LoadLanguagesAndBuildLanguageMenu();
+            RefreshLanguageMenuAndLanguageSetting(ResetLanguageResource(GetSettedOrSystemLanguage()));
         }
 
         protected override void OnSourceInitialized(EventArgs e)
@@ -34,31 +36,90 @@ namespace Chinese_Word_Analyzer
             IconRemover.SetWindowLong(hwnd, IconRemover.GWL_STYLE, IconRemover.GetWindowLong(hwnd, IconRemover.GWL_STYLE) & ~IconRemover.WS_SYSMENU);
         }
 
-        private void ResetLanguageResource(ResourceDictionary NewLanguageResource)
+        //load region codes to application dictionary
+        private void LoadRegionCodes()
+        {
+            App.Current.Resources.MergedDictionaries.Add(new ResourceDictionary() { Source = new Uri("LanguageResource/regioncodes.xaml", UriKind.Relative) });
+        }
+
+        //load language list to application dictionary and build language menu
+        private void LoadLanguagesAndBuildLanguageMenu()
+        {
+            var LanguageResourceConfig = new ResourceDictionary() { Source = new Uri("LanguageResource/languages.xaml", UriKind.Relative) };
+            foreach (var key in LanguageResourceConfig.Keys)
+            {
+                var addMe = new MenuItem();
+                addMe.Header = key;
+                addMe.Click += new RoutedEventHandler(this.LanguageControlClick);
+                LanguageMenu.Items.Insert(0, addMe);
+            }
+            App.Current.Resources.MergedDictionaries.Add(LanguageResourceConfig);
+        }
+
+        private static string GetSystemLanguageResourceKey()
+        {
+            string LanguageResourceKey = App.Current.TryFindResource(System.Globalization.CultureInfo.InstalledUICulture.Name) as string;
+            if (LanguageResourceKey == null)
+                LanguageResourceKey = "Default";
+            return LanguageResourceKey;
+        }
+
+        private static Tuple<string, ResourceDictionary> GetLanguage(string LanguageResourceKey)
+        {
+            return new Tuple<string, ResourceDictionary>(LanguageResourceKey, new ResourceDictionary() { Source = new Uri(App.Current.TryFindResource(LanguageResourceKey) as string, UriKind.Relative) });
+        }
+
+        //获得用户已设定的语言，如果用户没有指定，则使用默认语言(取决于操作系统语言)
+        private static Tuple<string, ResourceDictionary> GetSettedOrSystemLanguage()
+        {
+            string LanguageResourceKey = Properties.Settings.Default.LanguageResourceKey as string;
+            if (string.IsNullOrEmpty(LanguageResourceKey))
+                LanguageResourceKey = GetSystemLanguageResourceKey();
+            return GetLanguage(LanguageResourceKey);
+        }
+
+        //remove old language resource then add new language resource to application dictionary
+        //returns language's DisplayName
+        private string ResetLanguageResource(Tuple<string, ResourceDictionary> NewLanguage)
         {
             if (CurrentLanguageResource != null)
                 App.Current.Resources.MergedDictionaries.Remove(CurrentLanguageResource);
-            App.Current.Resources.MergedDictionaries.Add(NewLanguageResource);
+            App.Current.Resources.MergedDictionaries.Add(NewLanguage.Item2);
 
-            CurrentLanguageResource = NewLanguageResource;
+            CurrentLanguageResource = NewLanguage.Item2;
+            return NewLanguage.Item1;
         }
 
-        private static ResourceDictionary GetDefaultLanguageResource()
+        //设置语言菜单中当前已选的语言，并设置程序Setting
+        private void RefreshLanguageMenuAndLanguageSetting(string LanguageResourceKey)
         {
-            string SystemLanguage = System.Globalization.CultureInfo.InstalledUICulture.Name;
-            SystemLanguage = "en-US";
+            foreach (object p in LanguageMenu.Items)
+                if (p is MenuItem)
+                    (p as MenuItem).IsChecked = false;
 
-            if (SystemLanguage.Equals("zh-CN"))
-                return new ResourceDictionary() { Source = new Uri(lang_zh_chs, UriKind.RelativeOrAbsolute) };
-            if (SystemLanguage.IndexOf("zh") != -1)
-                return new ResourceDictionary() { Source = new Uri(lang_zh_cht, UriKind.RelativeOrAbsolute) };
-            return new ResourceDictionary() { Source = new Uri(lang_en_us, UriKind.RelativeOrAbsolute) };
+            foreach (object p in LanguageMenu.Items)
+                if (p is MenuItem)
+                    if ((p as MenuItem).Header.Equals(LanguageResourceKey))
+                        (p as MenuItem).IsChecked = true;
+
+            Properties.Settings.Default.LanguageResourceKey = LanguageResourceKey;
         }
 
-        private ResourceDictionary CurrentLanguageResource;
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
 
-        const string lang_zh_chs = "LanguageResource/lang_zh-chs.xaml";
-        const string lang_zh_cht = "LanguageResource/lang_zh-cht.xaml";
-        const string lang_en_us = "LanguageResource/lang_en-us.xaml";
+        private void LanguageControlClick(object sender, RoutedEventArgs e)
+        {
+            RefreshLanguageMenuAndLanguageSetting(ResetLanguageResource(GetLanguage((sender as MenuItem).Header as string)));
+        }
+
+        private void UseSystemLanguage_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshLanguageMenuAndLanguageSetting(ResetLanguageResource(GetLanguage(GetSystemLanguageResourceKey())));
+        }
+
+        public ResourceDictionary CurrentLanguageResource { get; private set; }
     }
 }
