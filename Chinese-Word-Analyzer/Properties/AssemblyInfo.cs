d17243395a5a72884fcab7de6c2839232d91a5d8
liuziangexit <liuziang@liuziangexit.com>
2018-08-14 19:41:36 +0800

    更新程序集设定

--- Chinese-Word-Analyzer/Properties/AssemblyInfo.cs
+++ Chinese-Word-Analyzer/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Chinese-Word-Analyzer")]
+[assembly: AssemblyTitle("Chinese Word Analyzer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Chinese-Word-Analyzer")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("liuziangexit.com")]
+[assembly: AssemblyProduct("Chinese Word Analyzer")]
+[assembly: AssemblyCopyright("Copyright © liuziangexit.com 2018.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
